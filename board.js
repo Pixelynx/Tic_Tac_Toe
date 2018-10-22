551d99d21fdba2f359ba276a8612e839ffa72471
Pixelynx <tjstevens1992@gmail.com>
2018-10-22 12:35:40 -0400

    board done!

---
+++ board.js
@@ -0,0 +1,37 @@
+class Board {
+  constructor() {
+    this.grid = [
+      [' ', ' ', ' '],
+      [' ', ' ', ' '],
+      [' ', ' ', ' ']
+    ];
+  };
+
+// Sets new cleared board
+
+printLine() {
+  console.log("+-----------+");
+}
+
+printRow(row) {
+  this.row = row;
+  console.log(row.join("  | "));
+}
+
+setBoard() {
+  this.printLine();
+  this.printRow(this.grid[0]);
+  this.printLine();
+  this.printRow(this.grid[1]);
+  this.printLine();
+  this.printRow(this.grid[2]);
+  this.printLine();
+}
+
+};
+
+// Updates board when move is made
+
+// Check if space on board is available
+let board = new Board();
+board.setBoard();
